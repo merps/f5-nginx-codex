dfe973654a89559f636c2ff25a836c27a6616c3d
Michael Kennedy <merpso@gmail.com>
2020-10-14 14:23:04 +1100

    added added ALB IAM, etc

--- src/main.tf
+++ src/main.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = "ap-south-1"
-}
-
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_id
 }
@@ -21,10 +17,6 @@ provider "kubernetes" {
 data "aws_availability_zones" "available" {
 }
 
-locals {
-  cluster_name = "my-cluster"
-}
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.47.0"
@@ -39,12 +31,12 @@ module "vpc" {
   enable_dns_hostnames = true
 
   public_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
     "kubernetes.io/role/elb"                      = "1"
   }
 
   private_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
     "kubernetes.io/role/internal-elb"             = "1"
   }
 }
@@ -53,7 +45,7 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "12.2.0"
 
-  cluster_name    = "${local.cluster_name}"
+  cluster_name    = var.cluster_name
   cluster_version = "1.17"
   subnets         = module.vpc.private_subnets
 
@@ -78,4 +70,42 @@ module "eks" {
 
   write_kubeconfig   = true
   config_output_path = "./"
+  workers_additional_policies = [aws_iam_policy.worker_policy.arn]
+}
+
+resource "aws_iam_policy" "worker_policy" {
+  name        = "worker-policy"
+  description = "Worker policy for the ALB Ingress"
+
+  policy = file("iam-policy.json")
+}
+
+provider "helm" {
+  version = "1.3.1"
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+    load_config_file       = false
+  }
+}
+
+resource "helm_release" "ingress" {
+  name       = "ingress"
+  chart      = "aws-alb-ingress-controller"
+  repository = "http://storage.googleapis.com/kubernetes-charts-incubator"
+  version    = "1.0.2"
+
+  set {
+    name  = "autoDiscoverAwsRegion"
+    value = "true"
+  }
+  set {
+    name  = "autoDiscoverAwsVpcID"
+    value = "true"
+  }
+  set {
+    name  = "clusterName"
+    value = var.cluster_name
+  }
 }
---
+++ src/vars.tf
@@ -0,0 +1,4 @@
+variable "cluster_name" {
+  description = "Kubentes Cluster Name"
+  default = "k8s_cluster"
+}
